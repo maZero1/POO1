721f7db82a590cb6acccb997f640ed366d4ba318
maZero1 <tkmarlon594@gmail.com>
2025-05-26 20:26:25 -0300

    finalizada a classe jogo

--- src/Catalogo/Jogo.java
+++ src/Catalogo/Jogo.java
@@ -1,4 +1,31 @@
 package Catalogo;
+import java.util.*;
 
 public class Jogo {
+    private int qtdJogadores;
+    private String plataforma;
+
+
+    public boolean setQtdJogadores(int jogadores){
+        if (jogadores >= 0 ){
+            this.qtdJogadores = jogadores;
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public int getQtdJogadores(){
+        return this.qtdJogadores;
+    }
+
+    public boolean setPlataforma(String plataforma) {
+        if (!plataforma.isEmpty()) {
+            this.plataforma = plataforma;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
