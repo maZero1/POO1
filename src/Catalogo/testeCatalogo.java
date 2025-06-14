43e37b4407604655b05d4ae6fe7f42c0d77484e1
maZero1 <tkmarlon594@gmail.com>
2025-06-14 09:10:41 -0300

    adicionada classe de teste de catalogo e o uso de superclasse

--- src/Catalogo/testeCatalogo.java
+++ src/Catalogo/testeCatalogo.java
@@ -15,6 +15,9 @@ public class testeCatalogo {
         Filme fil1 = new Filme("Star Wars", 120, "Top", "George Lucas");
         Filme fil2 = new Filme("Toy Story", 100, "Show", "John Lasseter");
 
+        Jogo jog1 = new Jogo("Returnal", 300, "Difícil", 2,"PS5");
+        Jogo jog2 = new Jogo("Ghost Reacon", 40,"Bom demais",4,"PS5");
+
         assertEquals("Titulo : Pulse, Duracao: 90, Comentario: Gostei, Artista: Pink Floyd, Faixas: 10", alb1.toString());
         assertEquals("Titulo : Abbey Road, Duracao: 50, Comentario: Bom, Artista: The Beatles, Faixas: 8", alb2.toString());
         //FAZER DPS PARA JOGOS
@@ -25,17 +28,21 @@ public class testeCatalogo {
         cat1.addItem(alb2);
         cat1.addItem(fil1);
         cat1.addItem(fil2);
+        cat1.addItem(jog1);
+        cat1.addItem(jog2);
 
         //obter os itens do catalogo
         ArrayList<Item> colecao = cat1.getItens();
 
         //verificar tamanho da lista
-        assertEquals(4, colecao.size());
+        assertEquals(6, colecao.size());
 
         //verificar conteudo da lista
         assertEquals(alb1, colecao.get(0));
         assertEquals(alb2, colecao.get(1));
         assertEquals(fil1, colecao.get(2));
         assertEquals(fil2, colecao.get(3));
+        assertEquals(jog1, colecao.get(4));
+        assertEquals(jog2, colecao.get(5));
     }
 }
