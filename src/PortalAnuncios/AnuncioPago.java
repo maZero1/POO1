9e7df014a9b0bcc55da997bef1929e6ff2f6ff40
maZero1 <tkmarlon594@gmail.com>
2025-07-07 09:43:07 -0300

    classes quase prontas

---
+++ src/PortalAnuncios/AnuncioPago.java
@@ -0,0 +1,12 @@
+package PortalAnuncios;
+
+public class AnuncioPago extends Anuncio {
+    private int quantidadeDias;
+    private double valorPago;
+
+    public AnuncioPago(String produto, double valorVenda, String cidade, String estado, int quantidadeDias, double valorPago){
+        super(produto, valorVenda, cidade, estado);
+        this.quantidadeDias = quantidadeDias;
+        this.valorPago = valorPago;
+    }
+}
